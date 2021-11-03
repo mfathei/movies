892847c0c22e593f8ff246de604905f3e3f70052
Mohamed Mahdy <mfathei@outlook.com>
2021-11-03 15:45:40 +0200

    feature/movies-module: add movies env variables and keys

--- Modules/Movies/Config/config.php
+++ Modules/Movies/Config/config.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-    'name' => 'Movies'
+    'name' => 'Movies',
+    'api_url' => env('MOVIES_API_URL', 'https://api.themoviedb.org/3/'),
+    'api_key' => env('MOVIES_API_KEY', ''),
 ];
--- Modules/Movies/Routes/web.php
+++ Modules/Movies/Routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Movies\Http\Controllers\MoviesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('movies')->group(function() {
-    Route::get('/', 'MoviesController@index');
+    Route::get('/', [MoviesController::class, 'index']);
 });
