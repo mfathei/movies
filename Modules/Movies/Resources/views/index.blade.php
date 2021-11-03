991273d7284b41a0db193ba3b82e77727b003ad9
Mohamed Mahdy <mfathei@outlook.com>
2021-11-03 12:33:12 +0200

    feature/movies-module: create Movies module

---
+++ Modules/Movies/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('movies::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('movies.name') !!}
+    </p>
+@endsection
