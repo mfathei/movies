4b6499d964a6c04de0fd74ebe6ed973feece12ce
Mohamed Mahdy <mfathei@outlook.com>
2021-11-07 01:16:23 +0200

    feature/movies-apis: create ListMoviesAction and DependencyServiceProvider and use ADR pattern

---
+++ Modules/Movies/Http/Controllers/Apis/Actions/ListMoviesAction.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Modules\Movies\Http\Controllers\Apis\Actions;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use Modules\Movies\Entities\Movie;
+use Modules\Movies\Http\Controllers\Apis\Responders\ResponderInterface;
+use Modules\Movies\Http\Requests\ListMoviesRequest;
+
+class ListMoviesAction
+{
+    public const SORTABLE_FIELDS = [
+        'popular' => 'popularity',
+        'rated' => 'vote_average',
+    ];
+
+    /**
+     * @var \Modules\Movies\Http\Controllers\Apis\Responders\ResponderInterface
+     */
+    protected $responder;
+    /**
+     * @var \Modules\Movies\Http\Requests\ListMoviesRequest
+     */
+    protected $request;
+
+    public function __construct(ListMoviesRequest $request, ResponderInterface $responder)
+    {
+        $this->request = $request;
+        $this->responder = $responder;
+    }
+
+    public function __invoke()
+    {
+        $query = Movie::query()->when($this->request->category_id, function ($q, $category_id) {
+            return $q->whereHas('genres', function ($query) use ($category_id) {
+                return $query->where('genres.id', $category_id);
+            });
+        });
+
+        $this->parseSortParameters($this->request)->each(function ($order) use ($query) {
+            $query->orderBy($order['key'], $order['value']);
+        });
+
+        return $this->responder->send($query->get());
+    }
+
+    protected function parseSortParameters(Request $request): Collection
+    {
+        $orderBy = collect();
+        collect($request->all())->each(function ($param, $key) use ($orderBy) {
+            if (count($field = explode('|', $key)) > 1 && $newName = $this->replaceSortField($field[0])) {
+                $orderBy->push(['key' => $newName, 'value' => $field[1] ?? 'asc']);
+            }
+        });
+
+        return $orderBy;
+    }
+
+    protected function replaceSortField(string $name): ?string
+    {
+        return self::SORTABLE_FIELDS[$name] ?? null;
+    }
+}
--- Modules/Movies/Routes/api.php
+++ Modules/Movies/Routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Modules\Movies\Http\Controllers\Apis\Actions\ListMoviesAction;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/movies', function (Request $request) {
-    return $request->user();
-});
+Route::get('/movies', ListMoviesAction::class);
