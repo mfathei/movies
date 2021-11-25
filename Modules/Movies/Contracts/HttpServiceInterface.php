c83c5d0c7c5c4b39c935f167958d4362dcceb74c
Mohamed Mahdy <mfathei@outlook.com>
2021-11-25 21:52:20 +0200

    feature/mocking: add ImportMoviesTest and use api mock

--- Modules/Movies/Contracts/HttpServiceInterface.php
+++ Modules/Movies/Contracts/HttpServiceInterface.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace Modules\Movies\Contracts;
 
-use GuzzleHttp\Client;
-
 interface HttpServiceInterface
 {
-    public function __construct(Client $client, ResponseDecoderInterface $responseDecoder, string $baseUri, array $headers = []);
-
     public function getData(): mixed;
 
     public function setBaseUri(string $baseUri): self;
--- Modules/Movies/Tests/Unit/Jobs/ImportMoviesTest.php
+++ Modules/Movies/Tests/Unit/Jobs/ImportMoviesTest.php
@@ -1,17 +1,30 @@
 <?php
 
-declare (strict_types = 1);
+declare(strict_types=1);
 
 namespace Modules\Movies\Tests\Unit\Jobs;
 
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Bus;
+use Modules\Movies\Contracts\HttpServiceInterface;
 use Modules\Movies\Contracts\MoviesRepositoryInterface;
+use Modules\Movies\Contracts\ResponseDecoderInterface;
+use Modules\Movies\Entities\Movie;
 use Modules\Movies\Jobs\ImportMovies;
+use Modules\Movies\Repositories\MoviesRepository;
+use Modules\Movies\Services\HttpService;
 use Modules\Movies\Utilities\ManagesIntervalRun;
 use Tests\TestCase;
 
-class ImportMoviesTest extends TestCase
+/**
+ * @internal
+ * @coversNothing
+ */
+final class ImportMoviesTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function testImportMoviesIsDispatched(): void
     {
         // Mock
@@ -22,8 +35,8 @@ class ImportMoviesTest extends TestCase
 
         $intervalRunMock->method('getNextRun')->willReturn(now()->addDays());
 
-        $repositoryMock = $this->getMockBuilder(MoviesRepositoryInterface::class)
-            ->onlyMethods(['updateOrInsertMovie', 'syncGenres', 'filterMoviesByGenres', 'addSortColumns', 'getQuery'])
+        $repositoryMock = $this->getMockBuilder(MoviesRepository::class)
+            ->onlyMethods(['updateOrInsertMovie', 'syncGenres'])
             ->getMock();
 
         // Dispatch
@@ -31,5 +44,37 @@ class ImportMoviesTest extends TestCase
 
         // Assert
         Bus::assertDispatched(ImportMovies::class);
+    }
+
+    public function testImportMoviesWorksWithMock(): void
+    {
+        // Mock
+        $intervalRunMock = $this->getMockBuilder(ManagesIntervalRun::class)
+            ->onlyMethods(['getNextRun', 'setLastExecutionTime'])
+            ->getMock();
+
+        $intervalRunMock->method('getNextRun')->willReturn(now()->addDays());
+        $this->app->instance(ManagesIntervalRun::class, $intervalRunMock);
+
+        $httpServiceMock = $this->getMockBuilder(HttpService::class)
+            ->onlyMethods(['getData'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $response = file_get_contents(__DIR__.'/../../Mocks/mocks-movies.json');
+        $httpServiceMock->method('getData')->willReturn($this->app->make(ResponseDecoderInterface::class)->decode(new Response(200, [], $response)));
+        $this->app->instance(HttpServiceInterface::class, $httpServiceMock);
+
+        // Dispatch
+        $job = new ImportMovies(1, app(ManagesIntervalRun::class), app(MoviesRepositoryInterface::class));
+        $job->handle();
+
+        // Assert
+        static::assertSame(3, Movie::all()->count());
+        tap(Movie::findOrFail(580489), function ($movie) {
+            $this->assertFalse($movie->adult);
+            $this->assertSame('/70nxSw3mFBsGmtkvcs91PbjerwD.jpg', $movie->backdrop_path);
+            $this->assertSame('Updated to diffrentiate between mocking and real data', $movie->original_title);
+        });
     }
 }
